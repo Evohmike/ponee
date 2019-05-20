75fa421514d5940e1b0e13d5af8c6a32e8d5f728
valentinogagliardi <valentino@valentinog.com>
2019-05-20 17:50:21 +0200

    configure prod env for Heroku

---
+++ configure_prod_envs.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+import subprocess
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV = '.env.prod'
+ENV_PATH = os.path.join(BASE_DIR, ENV)
+HEROKU = 'heroku'
+HEROKU_TOOLBELT = shutil.which(HEROKU)
+GIT_REPO = os.path.join(BASE_DIR, '.git')
+
+
+def config_set():
+    if not HEROKU_TOOLBELT:
+        raise FileNotFoundError('Heroku toolbelt not found. Install it and try again.')
+    if not os.path.isfile(ENV_PATH):
+        raise FileNotFoundError(f'{ENV} does not exists. Create the file and try again')
+    if not os.path.exists(GIT_REPO):
+        raise OSError(f'{GIT_REPO} is not a valid Git repo remote. Create a Git repo and try again')
+    with open(ENV_PATH, encoding='utf-8') as fp:
+        for _, line in enumerate(fp):
+            subprocess.run([HEROKU_TOOLBELT, "config:set", line.split()])
+
+
+if __name__ == '__main__':
+    config_set()
